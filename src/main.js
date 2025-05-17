3d5000f5384d42367bbe242eeb13d8418944d1c4
LiamStammeleerEhb <liam.stammeleer@student.ehb.be>
2025-05-17 11:00:45 +0200

    test

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import '../src/scss/style.scss'
+import * as bootstrap from 'bootstrap'
+
+// In PHP heb je een "include" functie... In javascript niet. Dus heb ik deze methode gebruikt
+import { api } from './api.js'
+
+// Start de api code
+api();

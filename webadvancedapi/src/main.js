075b6523b34da6bb4fea87ef1a01064f7c5e4711
LiamStammeleerEhb <liam.stammeleer@student.ehb.be>
2025-05-17 10:56:14 +0200

    Delete webadvancedapi directory

--- webadvancedapi/src/main.js
+++
@@ -1,9 +0,0 @@
-import './style.css'
-import '../src/scss/style.scss'
-import * as bootstrap from 'bootstrap'
-
-// In PHP heb je een "include" functie... In javascript niet. Dus heb ik deze methode gebruikt
-import { api } from './api.js'
-
-// Start de api codee
-api();

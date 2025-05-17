543cc9739ee908d3e043399c702028b159d99c94
LiamStammeleerEhb <liam.stammeleer@student.ehb.be>
2025-05-17 10:36:35 +0200

    Updated main.js comments

--- webadvancedapi/src/main.js
+++ webadvancedapi/src/main.js
@@ -2,4 +2,8 @@ import './style.css'
 import '../src/scss/style.scss'
 import * as bootstrap from 'bootstrap'
 
+// In PHP heb je een "include" functie... In javascript niet. Dus heb ik deze methode gebruikt
+import { api } from './api.js'
+
+// Start de api code
 api();

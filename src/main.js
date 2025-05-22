d32b504ba8581a3cd57b69e03beb8f9b9ec5c012
LiamStammeleerEhb <liam.stammeleer@student.ehb.be>
2025-05-22 10:30:32 +0200

    Less css on the indexpage

--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
-import './style.css'
-<<<<<<< HEAD
-import '../src/scss/style.scss'
-import * as bootstrap from 'bootstrap'
-
 // In PHP heb je een "include" functie... In javascript niet. Dus heb ik deze methode gebruikt
 import { api } from './api.js'
-
-// Start de api code
-=======
-import { api } from './api.js'
 import '../src/scss/style.scss'
+import './style.css'
 import * as bootstrap from 'bootstrap'
 
->>>>>>> 075b6523b34da6bb4fea87ef1a01064f7c5e4711
 api();
